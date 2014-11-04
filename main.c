385e317b90faccf941c78b6e56413eb446116137
Per Lundh <plundh@perlundh.com>
2014-11-04 13:03:45 +0100

    Add new method of testing using main.c

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include "object.h"
 
 #include <stdio.h>
+#include <assert.h>
 
 int main(int argc, char** argv)
 {
-	object* root = create_root(1000000);
+	object* root = create_root(10000000);
+	assert(root != NULL);
 
 	printf("Root and pool allocated\n");
 	
-	int nobjs = 5000;
-	object* pobj = root;
-	for(int co = 0; co < nobjs; co++){
-		pobj = create_object(pobj);
+	int ntrees = 1000;
+	object* treepar = root;
+	for(int to = 0; to < ntrees; to++){
+		int nobjs = 500;
+		object* pobj = treepar;
+		for(int co = 0; co < nobjs; co++){
+			pobj = create_object(pobj);
+		}
+		treepar = treepar->children[0];
 	}
 
 	printf("Object structure created\n");
