1e61a40931cad2277926a5a5e002aa9772ee17b0
Per Lundh <plundh@perlundh.com>
2014-11-12 00:53:03 +0100

    Shit

--- data/input_config.lua
+++ data/input_config.lua
@@ -2,3 +2,4 @@
 -- name | invert? | deadzone, negative | positive | maximum, negative | positive
 input.create("horizontal", false, 0.1, 0.1, 1.0, 1.0)
 input.create("vertical", false, 0.1, 0.1, 1.0, 1.0)
+
