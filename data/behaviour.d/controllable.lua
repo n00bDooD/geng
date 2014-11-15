e1358c08cf04ca719f4f2f060423b362fdb1cacb
Per Lundh <plundh@perlundh.com>
2014-11-15 18:50:28 +0100

    Less annoying behaviour

--- data/behaviour.d/controllable.lua
+++ data/behaviour.d/controllable.lua
@@ -2,10 +2,8 @@ local xacc = 2
 local yacc = 1
 
 function update(obj, current_time)
-	local horinp = input.get('horizontal')
-	local vertinp = input.get('vertical')
-
-	io.write(string.format("Force (%f,%f)", horinp, vertinp) .. '\n')
+	local horinp = input.get('horizontal') * xacc
+	local vertinp = input.get('vertical') * yacc
 
 	obj:apply_force(vector.new(horinp,
 				   vertinp))
