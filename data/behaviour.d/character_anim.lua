ef87b54222d29fe33284b5abeb0123ba254f7797
Per Lundh <plundh@perlundh.com>
2014-12-09 00:10:04 +0100

    Add kenney platformer and do some platformer-stuff

---
+++ data/behaviour.d/character_anim.lua
@@ -0,0 +1,45 @@
+local duck 	= args[1]
+local front 	= args[2]
+local hurt 	= args[3]
+local jump 	= args[4]
+
+local walk_cycle = args[5]
+local walk_speed = 8
+
+local object_state = 'walk'
+local walk_idx = 1
+
+local next_step = 0
+local cur_step = 0
+function scene_update(obj, step)
+	cur_step = cur_step + step
+	if cur_step > next_step then
+		next_step = math.ceil(cur_step)
+		local r = math.random()
+		if r > 0.3 then 
+			object_state = 'walk'
+		elseif r > 0.2 then
+			object_state = 'duck'
+		elseif r > 0.1 then
+			object_state = 'duck'
+		else
+			object_state = 'idle'
+		end
+	end
+
+	if object_state == 'idle' then
+		obj:set_sprite(front)
+	elseif object_state == 'duck' then
+		obj:set_sprite(duck)
+	elseif object_state == 'jump' then
+		obj:set_sprite(jump)
+	elseif object_state == 'hurt' then
+		obj:set_sprite(hurt)
+	elseif object_state == 'walk' then
+		walk_idx = walk_idx + step * walk_speed
+		if walk_idx > #walk_cycle then
+			walk_idx = 1
+		end
+		obj:set_sprite(walk_cycle[math.min(math.floor(walk_idx), #walk_cycle)])
+	end
+end
--- data/scene_init.lua
+++ data/scene_init.lua
@@ -1,30 +1,16 @@
-scene.load_prefab_file("data/prefabs.d/ball.prefab.lua")
-scene.load_prefab_file("data/prefabs.d/glass_block.prefab.lua")
-scene.load_prefab_file("data/prefabs.d/metal.prefab.lua")
-scene.load_prefab_file("data/prefabs.d/block.prefab.lua")
-scene.load_prefab_file("data/prefabs.d/coin.lua")
-scene.load_prefab_file("data/prefabs.d/hip_to_be_square.lua")
+scene.load_prefab_file("data/prefabs.d/hero.lua")
+scene.load_prefab_file("data/prefabs.d/buddy.lua")
+scene.load_prefab_file("data/prefabs.d/baddie.lua")
 
 scene.load_behaviour_file("data/behaviour.d/controllable.lua")
-scene.load_behaviour_file("data/behaviour.d/upfloat.lua")
-scene.load_behaviour_file("data/behaviour.d/timeout.lua")
-scene.load_behaviour_file("data/behaviour.d/collision_logger.lua")
 scene.load_behaviour_file("data/behaviour.d/boolet.lua")
-scene.load_behaviour_file("data/behaviour.d/breakable.lua")
+scene.load_behaviour_file("data/behaviour.d/character_anim.lua")
 
-local block = scene.spawn_prefab("block")
-block:set_pos(200, -200)
+local hero = scene.spawn_prefab("hero")
+hero:set_pos(200, -200)
 
-block:set_angle(1)
+local hero = scene.spawn_prefab("buddy")
+hero:set_pos(300, -200)
 
-local ball = scene.spawn_prefab("ball")
-ball:set_pos(200, -70)
-
-local glass = scene.spawn_prefab("glass_block")
-glass:set_pos(300, -300)
-
-local metal = scene.spawn_prefab("metal")
-metal:set_pos(100, -300)
-
-local target = scene.spawn_prefab("hip_to_be_square")
-target:set_pos(350, -70)
+local hero = scene.spawn_prefab("baddie")
+hero:set_pos(400, -200)
