9c75370aa5b5066f81313c8c583b8c7e18ab6a39
Per Lundh <plundh@perlundh.com>
2014-12-08 19:21:58 +0100

    Change physics parameters for green block

--- data/prefabs.d/hip_to_be_square.lua
+++ data/prefabs.d/hip_to_be_square.lua
@@ -1,7 +1,7 @@
 local o = scene.newobject()
 o:set_sprite(6)
-o:enable_physics(0.1, 2)
+o:enable_physics(0.4, 2)
 local c = o:add_box_collider(70, 70)
 c:set_friction(12)
-c:set_elasticity(0.3)
+c:set_elasticity(0.2)
 return o
