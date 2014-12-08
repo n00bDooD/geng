36a87da93be656cd48a142f0ef9b92f6a9f3f774
Per Lundh <plundh@perlundh.com>
2014-12-08 20:44:49 +0100

    Implement box functions

--- lua/lua_box.c
+++ lua/lua_box.c
@@ -51,14 +51,92 @@ static int lua_box_containsvect(lua_State* l)
 	return 1;
 }
 
+static int lua_box_center(lua_State* l)
+{
+	cpBB* b = luaG_checkbox(l, 1);
+	cpVect* v = luaG_pushvect(l);
+	*v = cpBBCenter(*b);
+	return 1;
+}
+
+static int lua_box_area(lua_State* l)
+{
+	cpBB* b = luaG_checkbox(l, 1);
+	lua_pushnumber(l, cpBBArea(*b));
+	return 1;
+}
+
+static int lua_box_mergedarea(lua_State* l)
+{
+	cpBB* a = luaG_checkbox(l, 1);
+	cpBB* b = luaG_checkbox(l, 2);
+	lua_pushnumber(l, cpBBMergedArea(*a,*b));
+	return 1;
+}
+
+static int lua_box_merge(lua_State* l)
+{
+	cpBB* a = luaG_checkbox(l, 1);
+	cpBB* b = luaG_checkbox(l, 2);
+	cpBB* ret = luaG_pushbox(l);
+	*ret = cpBBMerge(*a, *b);
+	return 1;
+}
+
+static int lua_box_expand(lua_State* l)
+{
+	cpBB* a = luaG_checkbox(l, 1);
+	cpVect* b = luaG_checkvect(l, 2);
+	cpBB* ret = luaG_pushbox(l);
+	*ret = cpBBExpand(*a, *b);
+	return 1;
+}
+
+static int lua_box_segment_query(lua_State* l)
+{
+	cpBB* b = luaG_checkbox(l, 1);
+	cpVect* f = luaG_checkvect(l, 2);
+	cpVect* t = luaG_checkvect(l, 3);
+
+	lua_pushnumber(l, cpBBSegmentQuery(*b, *f, *t));
+	return 1;
+}
+
+static int lua_box_intersectssegment(lua_State* l)
+{
+	cpBB* b = luaG_checkbox(l, 1);
+	cpVect* f = luaG_checkvect(l, 2);
+	cpVect* t = luaG_checkvect(l, 3);
+
+	lua_pushboolean(l, cpBBIntersectsSegment(*b, *f, *t));
+	return 1;
+}
+
+static int lua_box_new(lua_State* l)
+{
+	double a = luaL_checknumber(l, 1);
+	double b = luaL_checknumber(l, 2);
+	double c = luaL_checknumber(l, 3);
+	double d = luaL_checknumber(l, 4);
+	cpBB* n = luaG_pushbox(l);
+	*n = cpBBNew(a, b, c, d);
+	return 1;
+}
+
 static const luaL_reg methods[] = {
+	{"new", lua_box_new},
 	{"intersects", lua_box_intersects},
+	{"intersects_segment", lua_box_intersectssegment},
 	{"contains", lua_box_contains},
 	{"contains_vect", lua_box_containsvect},
+	{"expand", lua_box_expand},
+	{"merge", lua_box_merge},
+	{"center", lua_box_center},
+	{"area", lua_box_area},
+	{"area_merged", lua_box_mergedarea},
+	{"segment_query", lua_box_segment_query},
 	{NULL, NULL}
 };
-
-
 
 static const luaL_reg meta_methods[] = {
 	{NULL, NULL}
