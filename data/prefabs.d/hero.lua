c17f355d9806e4b5ba96cdf2593681f800814d79
Per Lundh <plundh@perlundh.com>
2015-01-30 22:18:19 +0100

    Use variable

--- data/prefabs.d/hero.lua
+++ data/prefabs.d/hero.lua
@@ -1,10 +1,11 @@
 local o = scene.newobject()
 o:set_name('hero')
 o:enable_physics(0.1, 0);
+local friction = 10
 local c = o:add_circle_collider(30, vector.new(0, 18))
-c:set_friction(0.01)
+c:set_friction(friction)
 local c = o:add_circle_collider(30, vector.new(0, -15))
-c:set_friction(0.01)
+c:set_friction(friction)
 o:add_behaviour("character_anim", 1, 2, 3, 4, {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})
 o:add_behaviour("platformer", -45)
 return o
