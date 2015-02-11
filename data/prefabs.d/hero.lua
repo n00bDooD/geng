fea11a81cb4c630e25c9cdaabdaa4973d734aa32
Per Lundh <plundh@perlundh.com>
2015-02-11 17:42:18 +0100

    Set elasticity

--- data/prefabs.d/hero.lua
+++ data/prefabs.d/hero.lua
@@ -2,10 +2,13 @@ local o = scene.newobject()
 o:set_name('hero')
 o:enable_physics(0.1, 0);
 local friction = 10
+local elasticity = 0
 local c = o:add_circle_collider(30, vector.new(0, 18))
 c:set_friction(friction)
+c:set_elasticity(elasticity)
 local c = o:add_circle_collider(30, vector.new(0, -15))
 c:set_friction(friction)
+c:set_elasticity(elasticity)
 o:add_behaviour("character_anim", 1, 2, 3, 4, {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})
 o:add_behaviour("platformer", -45)
 return o
