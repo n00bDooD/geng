e92960540db7260ef73b59148407811ef56f6124
Per Lundh <plundh@perlundh.com>
2015-03-06 20:30:02 +0100

    Set more logical values for the bomb

--- data/prefabs.d/bomb.lua
+++ data/prefabs.d/bomb.lua
@@ -4,10 +4,10 @@ local elasticity = 0.25
 local o = scene.newobject()
 o:set_name('bomb')
 o:enable_physics(0.05, 0);
-local c = o:add_circle_collider(70, vector.new(70*0.5, 70*0.5))
+local c = o:add_circle_collider(35, vector.new(35*0.5, -35*0.5))
 c:set_friction(friction)
 c:set_elasticity(elasticity)
 
-o:add_behaviour('animation', {{name='flash', speed=50, frames={{sprite=67}, {sprite=68}}}}, 'flash')
+o:add_behaviour('animation', {{name='flash', speed=8, frames={{sprite=67}, {sprite=68}}}}, 'flash')
 
 return o
