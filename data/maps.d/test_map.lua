7f4218e4be0e2961b6da470d1dfcf32d89c70bb0
Per Lundh <plundh@perlundh.com>
2015-03-06 22:50:42 +0100

    Begin tiled test

---
+++ data/maps.d/test_map.lua
@@ -0,0 +1,741 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.11.0",
+  orientation = "orthogonal",
+  width = 30,
+  height = 15,
+  tilewidth = 70,
+  tileheight = 70,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "grass",
+      firstgid = 1,
+      tilewidth = 70,
+      tileheight = 70,
+      spacing = 0,
+      margin = 0,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tiles = {
+        {
+          id = 0,
+          image = "data/textures/maptiles/grass/grassHalf.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 1,
+          image = "data/textures/maptiles/grass/grassHalfLeft.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 2,
+          image = "data/textures/maptiles/grass/grassHalfMid.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 3,
+          image = "data/textures/maptiles/grass/grassHalfRight.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 4,
+          image = "data/textures/maptiles/grass/slice01_01.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 5,
+          image = "data/textures/maptiles/grass/slice02_02.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 6,
+          image = "data/textures/maptiles/grass/slice03_03.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 7,
+          image = "data/textures/maptiles/grass/slice04_04.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 8,
+          image = "data/textures/maptiles/grass/slice05_05.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 9,
+          image = "data/textures/maptiles/grass/slice06_06.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 10,
+          image = "data/textures/maptiles/grass/slice07_07.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 11,
+          image = "data/textures/maptiles/grass/slice12_12.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 12,
+          image = "data/textures/maptiles/grass/slice13_13.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 13,
+          image = "data/textures/maptiles/grass/slice14_14.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 14,
+          image = "data/textures/maptiles/grass/slice15_15.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 15,
+          image = "data/textures/maptiles/grass/slice16_16.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 16,
+          image = "data/textures/maptiles/grass/slice17_17.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 17,
+          image = "data/textures/maptiles/grass/slice18_18.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 18,
+          image = "data/textures/maptiles/grass/slice19_19.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 19,
+          image = "data/textures/maptiles/grass/slice20_20.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 20,
+          image = "data/textures/maptiles/grass/slice21_21.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 21,
+          image = "data/textures/maptiles/grass/slice22_22.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 22,
+          image = "data/textures/maptiles/grass/slice23_23.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 23,
+          image = "data/textures/maptiles/grass/slice24_24.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 24,
+          image = "data/textures/maptiles/grass/slice25_25.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 25,
+          image = "data/textures/maptiles/grass/slice26_26.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 26,
+          image = "data/textures/maptiles/grass/slice27_27.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 27,
+          image = "data/textures/maptiles/grass/slice28_28.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 28,
+          image = "data/textures/maptiles/grass/slice29_29.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 29,
+          image = "data/textures/maptiles/grass/slice30_30.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 30,
+          image = "data/textures/maptiles/grass/slice31_31.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 31,
+          image = "data/textures/maptiles/grass/slice32_32.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 32,
+          image = "data/textures/maptiles/grass/slice33_33.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 33,
+          image = "data/textures/maptiles/grass/slice34_34.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 34,
+          image = "data/textures/maptiles/grass/slice35_35.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 35,
+          image = "data/textures/maptiles/grass/slice36_36.tga",
+          width = 70,
+          height = 70
+        }
+      }
+    },
+    {
+      name = "sand",
+      firstgid = 37,
+      tilewidth = 70,
+      tileheight = 70,
+      spacing = 0,
+      margin = 0,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tiles = {
+        {
+          id = 0,
+          image = "data/textures/maptiles/sand/sandHalf.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 1,
+          image = "data/textures/maptiles/sand/sandHalfLeft.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 2,
+          image = "data/textures/maptiles/sand/sandHalfMid.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 3,
+          image = "data/textures/maptiles/sand/sandHalfRight.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 4,
+          image = "data/textures/maptiles/sand/slice01_01.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 5,
+          image = "data/textures/maptiles/sand/slice02_02.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 6,
+          image = "data/textures/maptiles/sand/slice03_03.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 7,
+          image = "data/textures/maptiles/sand/slice04_04.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 8,
+          image = "data/textures/maptiles/sand/slice05_05.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 9,
+          image = "data/textures/maptiles/sand/slice06_06.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 10,
+          image = "data/textures/maptiles/sand/slice07_07.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 11,
+          image = "data/textures/maptiles/sand/slice12_12.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 12,
+          image = "data/textures/maptiles/sand/slice13_13.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 13,
+          image = "data/textures/maptiles/sand/slice14_14.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 14,
+          image = "data/textures/maptiles/sand/slice15_15.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 15,
+          image = "data/textures/maptiles/sand/slice16_16.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 16,
+          image = "data/textures/maptiles/sand/slice17_17.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 17,
+          image = "data/textures/maptiles/sand/slice18_18.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 18,
+          image = "data/textures/maptiles/sand/slice19_19.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 19,
+          image = "data/textures/maptiles/sand/slice20_20.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 20,
+          image = "data/textures/maptiles/sand/slice21_21.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 21,
+          image = "data/textures/maptiles/sand/slice22_22.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 22,
+          image = "data/textures/maptiles/sand/slice23_23.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 23,
+          image = "data/textures/maptiles/sand/slice24_24.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 24,
+          image = "data/textures/maptiles/sand/slice25_25.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 25,
+          image = "data/textures/maptiles/sand/slice26_26.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 26,
+          image = "data/textures/maptiles/sand/slice27_27.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 27,
+          image = "data/textures/maptiles/sand/slice28_28.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 28,
+          image = "data/textures/maptiles/sand/slice29_29.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 29,
+          image = "data/textures/maptiles/sand/slice30_30.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 30,
+          image = "data/textures/maptiles/sand/slice31_31.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 31,
+          image = "data/textures/maptiles/sand/slice32_32.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 32,
+          image = "data/textures/maptiles/sand/slice33_33.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 33,
+          image = "data/textures/maptiles/sand/slice34_34.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 34,
+          image = "data/textures/maptiles/sand/slice35_35.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 35,
+          image = "data/textures/maptiles/sand/slice36_36.tga",
+          width = 70,
+          height = 70
+        }
+      }
+    },
+    {
+      name = "purple",
+      firstgid = 73,
+      tilewidth = 70,
+      tileheight = 70,
+      spacing = 0,
+      margin = 0,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tiles = {
+        {
+          id = 0,
+          image = "data/textures/maptiles/purple/slice01_01.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 1,
+          image = "data/textures/maptiles/purple/slice02_02.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 2,
+          image = "data/textures/maptiles/purple/slice03_03.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 3,
+          image = "data/textures/maptiles/purple/slice04_04.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 4,
+          image = "data/textures/maptiles/purple/slice05_05.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 5,
+          image = "data/textures/maptiles/purple/slice06_06.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 6,
+          image = "data/textures/maptiles/purple/slice07_07.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 7,
+          image = "data/textures/maptiles/purple/slice12_12.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 8,
+          image = "data/textures/maptiles/purple/slice13_13.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 9,
+          image = "data/textures/maptiles/purple/slice14_14.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 10,
+          image = "data/textures/maptiles/purple/slice15_15.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 11,
+          image = "data/textures/maptiles/purple/slice16_16.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 12,
+          image = "data/textures/maptiles/purple/slice17_17.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 13,
+          image = "data/textures/maptiles/purple/slice18_18.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 14,
+          image = "data/textures/maptiles/purple/slice19_19.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 15,
+          image = "data/textures/maptiles/purple/slice20_20.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 16,
+          image = "data/textures/maptiles/purple/slice21_21.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 17,
+          image = "data/textures/maptiles/purple/slice22_22.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 18,
+          image = "data/textures/maptiles/purple/slice23_23.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 19,
+          image = "data/textures/maptiles/purple/slice24_24.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 20,
+          image = "data/textures/maptiles/purple/slice25_25.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 21,
+          image = "data/textures/maptiles/purple/slice26_26.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 22,
+          image = "data/textures/maptiles/purple/slice27_27.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 23,
+          image = "data/textures/maptiles/purple/slice28_28.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 24,
+          image = "data/textures/maptiles/purple/slice29_29.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 25,
+          image = "data/textures/maptiles/purple/slice30_30.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 26,
+          image = "data/textures/maptiles/purple/slice31_31.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 27,
+          image = "data/textures/maptiles/purple/slice32_32.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 28,
+          image = "data/textures/maptiles/purple/slice33_33.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 29,
+          image = "data/textures/maptiles/purple/slice34_34.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 30,
+          image = "data/textures/maptiles/purple/slice35_35.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 31,
+          image = "data/textures/maptiles/purple/slice36_36.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 32,
+          image = "data/textures/maptiles/purple/strip_purple_01.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 33,
+          image = "data/textures/maptiles/purple/strip_purple_02.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 34,
+          image = "data/textures/maptiles/purple/strip_purple_03.tga",
+          width = 70,
+          height = 70
+        },
+        {
+          id = 35,
+          image = "data/textures/maptiles/purple/strip_purple_04.tga",
+          width = 70,
+          height = 70
+        }
+      }
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      name = "Tile Layer 1",
+      x = 0,
+      y = 0,
+      width = 30,
+      height = 15,
+      visible = true,
+      opacity = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 106, 108, 108, 108, 107, 0, 0, 0, 0, 105, 0, 0, 105, 0, 0, 0, 105, 0, 105, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 106, 108, 108, 108, 107, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 108, 108, 108, 107, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 38, 39, 39, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 47, 43, 43, 51, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 48, 43, 43, 43, 46, 0, 47, 54, 69, 69, 0, 0, 0, 0, 0,
+        7, 7, 7, 7, 7, 7, 7, 7, 7, 20, 0, 0, 0, 0, 0, 0, 62, 69, 69, 58, 43, 57, 69, 69, 69, 0, 0, 0, 0, 0,
+        33, 33, 33, 33, 33, 33, 33, 33, 33, 24, 0, 0, 0, 0, 37, 0, 0, 0, 69, 69, 69, 69, 69, 69, 0, 0, 0, 0, 0, 0
+      }
+    }
+  }
+}
---
+++ data/utils/tiled_loader.lua
@@ -0,0 +1,62 @@
+
+-- Resolve a tile data-value to the name of a brefab
+function resolve_gid(map, gid)
+	for _, tileset in ipairs(map.tilesets) do
+		local idx = (gid - tileset.firstgid)+1
+
+		if idx < 1 then
+			-- Not this one
+		elseif idx > #tileset.tiles then
+			-- woo, this one
+			return tileset.name .. '_' .. tileset.tiles[idx].id
+		end
+	end
+end
+
+-- Add a tilelayer
+function add_tilelayer(map, layer)
+	if layer.type ~= 'tilelayer' then return end
+
+	for col = 1, layer.width do
+		for row = 1, layer.height do
+			local tileid = layer.data[col + row]
+			if tileid > 0 then
+				local x = col * map.tilewidth
+				local y = row * map.tileheight
+
+				local newtile = scene.spawn_prefab(resolve_gid(map, tileid))
+				if layer.visible == false then newtile:set_sprite(0) end
+				newtile:set_pos(x, y)
+			end
+		end
+	end
+end
+
+
+--
+-- Transformation functions (load prefabs for tiles etc)
+--
+
+function transform_tileset(ts)
+	for i = 1, #ts.tiles do
+		local tile = ts.tiles[i]
+		scene.load_prefab(ts.name .. '_' .. tostring(tile.id), transform_tile(tile))
+	end
+end
+
+function transform_tile(t)
+	local tex = renderer.add_texture(t.image);
+	local sprite = renderer.add_sprite(tex, t.width * 0.5, t.height * 0.5, 0, 0, t.width, t.height)
+	return (function() local o = scene.new_object(); o:set_sprite(sprite); return o end)
+end
+
+
+function load_tiled_file(output)
+	for _, ts in ipairs(output.tilesets) do
+		transform_tileset(ts)
+	end
+	for _, l in ipairs(output.layers) do
+		add_tilelayer(output, l)
+	end
+end
+
