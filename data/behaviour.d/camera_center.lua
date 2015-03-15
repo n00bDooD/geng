9fe70c916640d4228a77e4494076138e90e21477
Per Lundh <plundh@perlundh.com>
2015-03-16 00:28:32 +0100

    Actual improvements to the camera

--- data/behaviour.d/camera_center.lua
+++ data/behaviour.d/camera_center.lua
@@ -1,5 +1,6 @@
 
 function update(obj, current_time)
 	local p = obj:pos()
-	renderer.set_cam_pos(-p:x(), p:y() + 500)
+	local view = renderer.viewport()
+	renderer.set_cam_pos(-p:x() + (view:width() * 0.5), p:y() - (view:height() * 0.5))
 end
