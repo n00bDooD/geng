fa7a046474a797085f07ead044b1a808f2eb0ce7
Per Lundh <plundh@perlundh.com>
2015-04-27 18:22:06 +0200

    Move the sfx/ folder to be a subfolder of data/

--- data/audio_config.lua
+++ data/audio_config.lua
@@ -1,6 +1,6 @@
 audio.open()
 audio.channels(10)
 
-audio.load_chunk('sfx/Pickup_Coin.wav')
-audio.load_chunk('sfx/Jump.wav')
+audio.load_chunk('data/sfx/Pickup_Coin.wav')
+audio.load_chunk('data/sfx/Jump.wav')
 
--- data/init.lua
+++ data/init.lua
@@ -3,7 +3,3 @@ scene.load_scene('tiledtest', loadfile('data/scenes.d/tiledtest.lua'))
 
 scene.set_scene('tiledtest')
 
---[[
-scene.load_scene('test', loadfile('tfight/test.lua'))
-scene.set_scene('test')
-]]
